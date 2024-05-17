627d44ac047095550ce5a4ca39f89c1d8c5e1257
Anton Sundqvist <anton.sundqvist@astemes.com>
2024-05-17 18:55:24 +0300

    Add install script for copying files to teststand public

---
+++ source/install_script.bat
@@ -0,0 +1,4 @@
+move "%1\Astemes_CSVReportGenerator.seq" "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator.seq"
+mkdir "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator"
+move "%1\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp" "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp"
+pause
