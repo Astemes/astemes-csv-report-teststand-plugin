eb7670c7a6993401a22c1a315072f6be1782ae71
Anton Sundqvist <anton.sundqvist@astemes.com>
2024-05-17 22:40:18 +0300

    add 64-bit to install script

--- source/install_script.bat
+++ source/install_script.bat
@@ -1,5 +1,11 @@
-move "%1\Astemes_CSVReportGenerator.seq" "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator.seq"
-move "%1\logo_alphasigma_icon.ico" "%TestStandPublic%\Components\Icons\logo_alphasigma_icon.ico"
+:: 32-bit
+copy "%1\Astemes_CSVReportGenerator.seq" "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator.seq"
+copy "%1\logo_alphasigma_icon.ico" "%TestStandPublic%\Components\Icons\logo_alphasigma_icon.ico"
 mkdir "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator"
-move "%1\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp" "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp"
+copy "%1\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp" "%TestStandPublic%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp"
+:: 64-bit
+copy "%1\Astemes_CSVReportGenerator.seq" "%TestStandPublic64%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator.seq"
+copy "%1\logo_alphasigma_icon.ico" "%TestStandPublic64%\Components\Icons\logo_alphasigma_icon.ico"
+mkdir "%TestStandPublic64%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator"
+copy "%1\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp" "%TestStandPublic64%\Components\Models\ModelPlugins\Astemes_CSVReportGenerator\astemes-csv-report-plugin.lvlibp"
 pause
